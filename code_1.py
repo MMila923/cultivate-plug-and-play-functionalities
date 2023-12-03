1285ab320d230ca499fe24dfdd356bc9fb2301e1
Mila <m9234434@gmail.com>
2023-12-03 20:17:19 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,43 @@
+    const data = generateRandomData();
+        console.log("Random Number: " + item);
+}
+    const data = [];
+
+    return data;
+    data.forEach(item => {
+    });
+main();
+
+    }
+function generateRandomData() {
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+    for (let i = 0; i < 10; i++) {
+}
+function main() {    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+
+    return data
+
+
+
+    for item in data:
+def main():
+    data = generate_random_data()
+import random
+if __name__ == "__main__":
+    main()
+        print(f"Random Number: {item}")def main():
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+
+import random
+def generate_random_data():
+if __name__ == "__main__":
+
+    for item in data:
+
+    data = generate_random_data()
+
+        print(f"Random Number: {item}")
